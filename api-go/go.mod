53414cde033b44ba3c4b0f2dd09b1178aca2254f
David Johnston <djohnston@cydarm.com>
2022-05-04 15:49:22 +1000

    Davids changes

--- api-go/go.mod
+++ api-go/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/swag v0.19.5 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/mailru/easyjson v0.0.0-20190626092158-b2ccc519800e // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- api-go/server.go
+++ api-go/server.go
@@ -12,6 +12,19 @@ import (
 	"github.com/getkin/kin-openapi/routers/gorillamux"
 )
 
+func getUser(http.ResponseWriter, *http.Request) {
+
+	// do our logic
+}
+
+func createUser(http.ResponseWriter, *http.Request) {
+
+}
+
+func getAllUsers(http.ResponseWriter, *http.Request) {
+
+}
+
 func main() {
 	ctx := context.Background()
 	loader := openapi3.Loader{Context: ctx}
@@ -27,23 +40,52 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
 		err := validateFunc(router, ctx, r, w)
 		if err != nil {
 			w.Write([]byte(err.Error()))
+
 			return
 		}
 	})
+
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
+// Return
+// Reponse Code
+// Content Type
+// Reponse Body
+func handleRequest(router routers.Router, httpReq *http.Request) {
+
+	route, pathParams, err := router.FindRoute(httpReq)
+
+	path := route.Path
+	method := route.Method
+
+	if path == "/users" && method == "POST" {
+		return
+	}
+
+	if path == "/users" && pathParams["username"] != "" && httpReq.Method == "GET" {
+		return
+	}
+
+	if path == "/users" && httpReq.Method == "GET" {
+		return
+	}
+
+}
+
 func validateFunc(router routers.Router, ctx context.Context, httpReq *http.Request, w http.ResponseWriter) error {
 	route, pathParams, err := router.FindRoute(httpReq)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		w.WriteHeader(http.StatusNotFound)
 		return fmt.Errorf("find route err: %v", err)
 	}
 	log.Println("Route:", route.Path)
@@ -54,10 +96,15 @@ func validateFunc(router routers.Router, ctx context.Context, httpReq *http.Requ
 		PathParams: pathParams,
 		Route:      route,
 	}
+
 	if err := openapi3filter.ValidateRequest(ctx, requestValidationInput); err != nil {
+		log.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return fmt.Errorf("validate request err: %v", err)
 	}
+
+	// actually process the request
+
 	log.Println("Path checked")
 	return nil
 }
