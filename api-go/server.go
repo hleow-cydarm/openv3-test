e219c4d43825d0a81bea6e2d626b26598ad78276
hleow.cydarm.com <hleow@cydarm.com>
2022-05-05 11:37:03 +1000

    update server file

--- api-go/server.go
+++ api-go/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,6 +12,11 @@ import (
 	"github.com/getkin/kin-openapi/routers"
 	"github.com/getkin/kin-openapi/routers/gorillamux"
 )
+
+type User struct {
+	Username string `json:"username" gorm:"username"`
+	Name     string `json:"name" gorm:"name"`
+}
 
 func main() {
 	ctx := context.Background()
@@ -40,6 +46,24 @@ func main() {
 	}
 }
 
+func handleRequest(route *routers.Route, httpReq *http.Request, pathParams map[string]string) (int, interface{}) {
+	path := route.Path
+	method := route.Method
+
+	if path == "/user" && method == http.MethodPost {
+		return http.StatusCreated, &User{Name: "created test user", Username: "created test userName"}
+	}
+
+	if path == "/user/{username}" && pathParams["username"] != "" && httpReq.Method == "GET" {
+		return http.StatusOK, &User{Name: "found user", Username: "found username " + pathParams["username"]}
+	}
+
+	if path == "/user" && httpReq.Method == "GET" {
+		return http.StatusOK, []*User{{Name: "user 1", Username: "user 1"}, {Name: "user 2", Username: "user 2"}}
+	}
+	return 500, nil
+}
+
 func validateFunc(router routers.Router, ctx context.Context, httpReq *http.Request, w http.ResponseWriter) error {
 	route, pathParams, err := router.FindRoute(httpReq)
 	if err != nil {
@@ -58,6 +82,33 @@ func validateFunc(router routers.Router, ctx context.Context, httpReq *http.Requ
 		w.WriteHeader(http.StatusBadRequest)
 		return fmt.Errorf("validate request err: %v", err)
 	}
+
+	respStatus, respData := handleRequest(route, httpReq, pathParams)
+
+	responseValidationInput := &openapi3filter.ResponseValidationInput{
+		RequestValidationInput: requestValidationInput,
+		Status:                 respStatus,
+		Header:                 http.Header{"Content-Type": []string{"application/json"}},
+	}
+	data, err := json.Marshal(respData)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return fmt.Errorf("error parsing json %v", err)
+	}
+	responseValidationInput.SetBodyBytes(data)
+	if err := openapi3filter.ValidateResponse(ctx, responseValidationInput); err != nil {
+		return fmt.Errorf("error validate response: %v", err)
+	}
 	log.Println("Path checked")
+	JsonResponse(w, respStatus, respData)
 	return nil
+}
+
+func JsonResponse(w http.ResponseWriter, status int, jsonData interface{}) {
+	if err := json.NewEncoder(w).Encode(jsonData); err != nil {
+		fmt.Errorf("error parsing json %v", err)
+		return
+	}
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
 }
